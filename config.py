b86be429cd4b43caac4a3a7e6adacaf63a6e4513
HILLELOH <hilleloh@gmail.com>
2023-04-24 20:51:41 +0300

    changes, clean code, work properly! (config added)

---
+++ config.py
@@ -0,0 +1,7 @@
+root, fig, ax, canvas, toolbar, shapes, label_widgets, side_panel, cid, circle_cid, selected_shape, start_drag_x,\
+    start_drag_y, press_cid, release_cid, motion_cid = [None] * 16
+
+x1, y1 = [None]*2
+
+circle_x, circle_y = [None]*2
+line_x, line_y = [None]*2
