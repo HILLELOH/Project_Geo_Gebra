dc60bb7257a54bd3a33a00a193e77aff1878a0b1
TomKoltunov <74180693+TomKoltunov@users.noreply.github.com>
2023-06-01 11:52:59 +0300

    add area, perimeter and convex hull to Line.py

--- Shapes/Line.py
+++ Shapes/Line.py
@@ -54,4 +54,14 @@ class Line(Shape):
 
     def set_hidden(self, b):
         self.hidden = b
+        
+    def area(self) -> float:
+        return 0
+
+    def perimeter(self) -> float:
+        distance = math.sqrt((self.p2.get_x() - self.p1.get_x()) ** 2 + (self.p2.get_y() - self.p2.get_y()) ** 2)
+        return distance
+
+    def convex_hull(self):
+        return self
 
